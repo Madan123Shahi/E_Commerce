d471b4abfe626aac4f98a38d559bbd055372996e
unknown <madanshahi8894@gmail.com>
2025-03-20 03:12:17 +0530

    Login

--- front_end/src/components/Navbar.jsx
+++ front_end/src/components/Navbar.jsx
@@ -23,88 +23,84 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="bg-gray-100 w-full  font-sans">
+    <div className="bg-gray-50 w-full  font-sans">
       {/* First Div: Logo, Search Bar, Login, Cart */}
-      <div className="flex justify-between items-center px-4 py-4 bg-white shadow-md">
+      <div className="flex justify-between items-center px-4 py-3 bg-white shadow-sm gap-4 ">
         {/* Logo */}
         <div>
           <img
             src={logo}
-            width="50"
+            width="48"
             alt="Logo"
-            className="rounded-md cursor-pointer"
+            className="rounded cursor-pointer hover:opacity-90 transition-opacity duration-200"
             onClick={() => navigate("/")}
           />
         </div>
 
         {/* Search Bar */}
-        <div className="hidden md:flex gap-2 w-full max-w-lg bg-gray-100 px-4 py-2 rounded-md border border-gray-300 ">
+        <div className="hidden md:flex gap-2 w-full max-w-xl bg-gray-100 px-4 py-2 rounded-md border border-gray-300 shadow-inner">
           <a href="#">
             <FontAwesomeIcon icon={faSearch} className="text-gray-500" />
           </a>
           <input
             type="text"
             placeholder="Search for Products Brands and More..."
-            className=" flex-1 outline-none bg-transparent text-sm "
+            className=" flex-1 outline-none bg-transparent text-lg font-medium "
           />
         </div>
+        <div className="flex items-center gap-20 text-lg font-medium text-gray-500 cursor-pointer  ">
+          {/* Login */}
+          <div className="flex gap-1 items-center hover:text-blue-500 ">
+            <FontAwesomeIcon
+              icon={faCircleUser}
+              onClick={() => navigate("/login")}
+            />
+            <span
+              className="hidden sm:inline"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </span>
+          </div>
 
-        {/* Login */}
-        <div className="flex gap-2 items-center">
-          <FontAwesomeIcon
-            icon={faCircleUser}
-            className="text-gray-500 cursor-pointer"
-            onClick={() => navigate("/login")}
-          />
-          <button
-            className="text-md font-medium  text-gray-700 hover:text-gray-950
-          "
-            onClick={() => navigate("/login")}
-          >
-            Login
-          </button>
-        </div>
+          {/* Signup */}
+          <div className="flex gap-1 items-center hover:text-blue-500 ">
+            <FontAwesomeIcon
+              icon={faUserPlus}
+              onClick={() => navigate("/signup")}
+            />
 
-        {/* Signup */}
-        <div className="flex gap-2 items-center">
-          <FontAwesomeIcon
-            icon={faUserPlus}
-            className="text-gray-500 cursor-pointer"
-            onClick={() => navigate("/signup")}
-          />
+            <span
+              className="hidden sm:inline"
+              onClick={() => navigate("/signup")}
+            >
+              Sign Up
+            </span>
+          </div>
 
-          <button
-            className="text-md font-medium  text-gray-700 hover:text-gray-950 "
-            onClick={() => navigate("/signup")}
-          >
-            Sign Up
-          </button>
-        </div>
+          {/* Cart */}
+          <div className="flex gap-1 items-center hover:text-blue-500 ">
+            <FontAwesomeIcon
+              icon={faShoppingCart}
+              onClick={() => navigate("/cart")}
+            />
 
-        {/* Cart */}
-        <div className="flex gap-2 items-center">
-          <FontAwesomeIcon
-            icon={faShoppingCart}
-            className="text-gray-500 cursor-pointer"
-            onClick={() => navigate("/cart")}
-          />
-
-          <button
-            className="hidden md:inline-block text-md font-medium text-gray-700  hover:text-gray-950"
-            onClick={() => navigate("/cart")}
-          >
-            Cart
-          </button>
+            <span
+              className="hidden md:inline"
+              onClick={() => navigate("/cart")}
+            >
+              Cart
+            </span>
+          </div>
         </div>
       </div>
       {/* <hr className="h-2 bg-gray-400 border-none" /> */}
       {/* Second Div: Product Categories */}
-      <div className="flex justify-between items-center  px-2 py-4 gap-20 shadow-sm my-1">
+      <div className="flex justify-between items-center px-4 py-4 gap-6 md:px-4 md:py-3 md:gap-4">
         {categories.map((category, index) => (
           <span
             key={index}
-            className="text-sm font-medium text-gray-700 hover:text-gray-950
-          cursor-pointer transition-colors"
+            className="text-lg font-semibold text-gray-500 hover:text-blue-500 cursor-pointer transition-colors duration-200 whitespace-nowrap"
           >
             {category}
           </span>
--- front_end/src/pages/Login.jsx
+++ front_end/src/pages/Login.jsx
@@ -1,15 +1,39 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faUser, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 const LoginPage = () => {
   const [phoneOrEmail, setPhoneOrEmail] = useState("");
 
   return (
-    <div className="flex justify-center items-center bg-gray-400 min-h-screen">
-      <div className="flex flex-col">
-        <FontAwesomeIcon icon={faUser} className=" mt-8 text-7xl" />
+    <div className="flex justify-center items-center bg-gray-300 min-h-screen">
+      <div className="flex flex-col items-center">
+        <FontAwesomeIcon icon={faUser} className="text-6xl text-gray-500" />
 
-        <p className="mt-2 text-2xl ">USER LOGIN</p>
+        <p className="mt-4 p-3 text-lg font-medium text-gray-500">USER LOGIN</p>
+        <div className="flex gap-3  px-4 py-2 text-gray-500  border border-gray-300 shadow-inner items-center bg-gray-100 mt-3">
+          <FontAwesomeIcon
+            icon={faUser}
+            className="border-r-2 border-r-white pr-3"
+          />
+
+          <input
+            type="text"
+            placeholder="Phone Or Email"
+            className=" flex-1 outline-none bg-transparent text-sm "
+          />
+        </div>
+        <div className="flex gap-3  px-4 py-2 text-gray-500  border border-gray-300 shadow-inner items-center bg-gray-100 mt-4">
+          <FontAwesomeIcon
+            icon={faEyeSlash}
+            className="border-r-2 border-r-white pr-3"
+          />
+
+          <input
+            type="text"
+            placeholder="Password"
+            className=" flex-1 outline-none bg-transparent text-sm "
+          />
+        </div>
       </div>
     </div>
   );
